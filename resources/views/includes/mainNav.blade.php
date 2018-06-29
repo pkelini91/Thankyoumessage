f10d58b13ee79a2dfb55bff7a5457e5e1e5328ff
peter <kelinipeter@gmail.com>
2018-06-28 21:02:02 -0400

    laravel auth setup for heroku

--- resources/views/includes/mainNav.blade.php
+++ resources/views/includes/mainNav.blade.php
@@ -7,8 +7,6 @@
             <li class="active"><a href="{{route('home')}}">Home</a></li>
             <li><a href="{{route('about')}}">About</a></li>
             <li><a href="{{route('contact')}}">Contact</a></li>
-            <li><a href="{{route('login')}}">Login</a></li>
-            <li><a href="{{route('register')}}">Regsiter</a></li>
 
 
 
